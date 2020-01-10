d2f4cbd0e4faaeb356e9a083ddf6556c460db1fe
52idora <756855220@qq.com>
2020-01-10 18:50:30 +0800

    第一

--- app.js
+++ app.js
@@ -27,6 +27,16 @@ app.use(bodyparser({
 app.use(jwtKoa({secret:config.sys.JWT_SECRET}).unless({
       path: [/^\/sysUser\/login/] //数组中的路径不需要通过jwt验证
 }))
+app.use(async (ctx, next)=> {
+  ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  if (ctx.method == 'OPTIONS') {
+    ctx.body = 200;
+  } else {
+    await next();
+  }
+});
 
 app.use(json())
 app.use(logger())
